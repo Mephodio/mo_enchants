ed63431e388dc8b45ae4f648bc50c87f58af68ee
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-10-23 12:10:41 -0400

    Fixed text components

--- src/main/java/net/ldm/mo_enchants/procedures/BloodthirstHelperProcedure.java
+++ src/main/java/net/ldm/mo_enchants/procedures/BloodthirstHelperProcedure.java
@@ -1,5 +1,7 @@
 package net.ldm.mo_enchants.procedures;
 
+import net.minecraft.network.chat.contents.LiteralContents;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,8 +22,7 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.resources.ResourceLocation;
-// TODO: Replace TextComponent with its 1.19.2 counterpart
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.BlockPos;
 
 import net.ldm.mo_enchants.init.MoEnchantsEnchantments;
@@ -50,10 +51,11 @@ public class BloodthirstHelperProcedure {
 				if (entity instanceof LivingEntity _entity)
 					_entity.hurt(new DamageSource("enchantment.bloodthirst").bypassArmor(), 5);
 				entity.getPersistentData().putBoolean("bloodthirstAbilityCooldown", (true));
-				if (entity instanceof LivingEntity _entity)
+				if (entity instanceof LivingEntity _entity) {
 					_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 240,
 							(int) (-1 + EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.BLOODTHIRST.get(),
-									(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)))));
+									_entity.getMainHandItem()))));
+				}
 				if (entity instanceof LivingEntity _entity)
 					_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 240, 0));
 				if (world instanceof Level _level) {
@@ -93,7 +95,7 @@ public class BloodthirstHelperProcedure {
 				}.start(world, 240);
 			} else {
 				if (entity instanceof Player _player && !_player.level.isClientSide())
-					_player.displayClientMessage(new TextComponent("\uFFFDcThis ability is on cooldown!"), (true));
+					_player.displayClientMessage(Component.translatable("cooldown.generic"), true);
 				if (world instanceof Level _level) {
 					if (!_level.isClientSide()) {
 						_level.playSound(null, new BlockPos(x, y, z),
