58affff199c8ac37fe637679877f414c8d143020
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-10-23 12:42:19 -0400

    Optimized code, fixed random, fixed class typo

--- src/main/java/net/ldm/mo_enchants/procedures/RockMendingHelperProcedure.java
+++ src/main/java/net/ldm/mo_enchants/procedures/RockMendingHelperProcedure.java
@@ -1,41 +1,30 @@
 package net.ldm.mo_enchants.procedures;
 
-import net.minecraft.util.RandomSource;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.event.level.BlockEvent;
-
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.core.BlockPos;
-
 import net.ldm.mo_enchants.init.MoEnchantsEnchantments;
-
-import javax.annotation.Nullable;
-
-import java.util.Random;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class RockMendingHelperProcedure {
 	@SubscribeEvent
 	public static void onBlockBreak(BlockEvent.BreakEvent event) {
-		execute(event, event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getPlayer());
+		execute(event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getPlayer());
 	}
 
-	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
-		execute(null, world, x, y, z, entity);
-	}
-
-	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+	private static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		double rand = 0;
+		double rand;
 		rand = Math.round(Math.random() * 10);
-		if (EnchantmentHelper.getItemEnchantmentLevel(MoEnchantsEnchantments.ROCK_MENDING.get(),
+		if (EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.ROCK_MENDING.get(),
 				(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) != 0
 				&& (world.getBlockState(new BlockPos(x, y, z))).getMaterial() == net.minecraft.world.level.material.Material.STONE
 				&& ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY))
@@ -43,7 +32,7 @@ public class RockMendingHelperProcedure {
 			if (rand < 4) {
 				{
 					ItemStack _ist = (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY);
-					if (_ist.hurt(-1, (RandomSource) new Random(), null)) {
+					if (_ist.hurt(-1, RandomSource.create(), null)) {
 						_ist.shrink(1);
 						_ist.setDamageValue(0);
 					}
@@ -51,7 +40,7 @@ public class RockMendingHelperProcedure {
 			} else if (rand < 7) {
 				{
 					ItemStack _ist = (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY);
-					if (_ist.hurt(-1, (RandomSource) new Random(), null)) {
+					if (_ist.hurt(-1, RandomSource.create(), null)) {
 						_ist.shrink(1);
 						_ist.setDamageValue(0);
 					}
@@ -59,7 +48,7 @@ public class RockMendingHelperProcedure {
 			} else if (rand < 9) {
 				{
 					ItemStack _ist = (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY);
-					if (_ist.hurt(-1, (RandomSource) new Random(), null)) {
+					if (_ist.hurt(-1, RandomSource.create(), null)) {
 						_ist.shrink(1);
 						_ist.setDamageValue(0);
 					}
