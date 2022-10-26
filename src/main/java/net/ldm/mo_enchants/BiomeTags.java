e73024744e2ee18f3636d7f7d9db805ee3c05a92
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-10-26 19:46:03 -0400

    Moved biome tag vars to BiomeTags.java

---
+++ src/main/java/net/ldm/mo_enchants/BiomeTags.java
@@ -0,0 +1,10 @@
+package net.ldm.mo_enchants;
+
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.biome.Biome;
+
+public class BiomeTags {
+    public static TagKey<Biome> IS_FROZEN = TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(MoEnchantsMod.MODID, "is_frozen"));
+}
