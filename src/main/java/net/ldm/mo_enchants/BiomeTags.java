99cd10eabf27038796868c3566e96288e75d205f
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-10-26 20:07:05 -0400

    Added IS_HOT

--- src/main/java/net/ldm/mo_enchants/BiomeTags.java
+++ src/main/java/net/ldm/mo_enchants/BiomeTags.java
@@ -7,4 +7,5 @@ import net.minecraft.world.level.biome.Biome;
 
 public class BiomeTags {
     public static TagKey<Biome> IS_FROZEN = TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(MoEnchantsMod.MODID, "is_frozen"));
+    public static TagKey<Biome> IS_HOT = TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(MoEnchantsMod.MODID, "is_hot"));
 }
