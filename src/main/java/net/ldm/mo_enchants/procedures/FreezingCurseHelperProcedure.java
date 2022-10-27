cc48b9616a15d8b247bdd2a88b9742517dcb09b5
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-10-26 20:07:19 -0400

    Fixed code, now uses biome tags

--- src/main/java/net/ldm/mo_enchants/procedures/FreezingCurseHelperProcedure.java
+++ src/main/java/net/ldm/mo_enchants/procedures/FreezingCurseHelperProcedure.java
@@ -1,11 +1,9 @@
 package net.ldm.mo_enchants.procedures;
 
+import net.ldm.mo_enchants.BiomeTags;
 import net.ldm.mo_enchants.init.MoEnchantsEnchantments;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -26,13 +24,13 @@ public class FreezingCurseHelperProcedure {
 		}
 	}
 
-	static TagKey<Biome> IS_FROZEN = TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation("mo_enchants", "is_frozen"));
-
 	private static void execute(LevelAccessor world, BlockPos pos, Player entity) {
 		if (entity == null)
 			return;
 		Holder<Biome> biome = world.getBiome(pos);
-		if (biome.is(IS_FROZEN) && (EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.FREEZING_CURSE.get(), entity.getItemBySlot(EquipmentSlot.HEAD)) >= 1)
+		System.out.println(EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.FREEZING_CURSE.get(), entity.getItemBySlot(EquipmentSlot.HEAD)));
+		if (biome.is(BiomeTags.IS_FROZEN)
+				&& (EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.FREEZING_CURSE.get(), entity.getItemBySlot(EquipmentSlot.HEAD)) >= 1)
 				|| (EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.FREEZING_CURSE.get(), entity.getItemBySlot(EquipmentSlot.CHEST)) >= 1)
 				|| (EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.FREEZING_CURSE.get(), entity.getItemBySlot(EquipmentSlot.LEGS)) >= 1)
 				|| (EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.FREEZING_CURSE.get(), entity.getItemBySlot(EquipmentSlot.FEET)) >= 1)) {
