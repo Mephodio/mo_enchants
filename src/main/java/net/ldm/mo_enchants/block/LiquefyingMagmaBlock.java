0cf089ea1b82093e78c9da810809055bd7f5319a
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-10-26 20:43:38 -0400

    Merged LiquefyingMagmaBlock added() and removed() functions into self

--- src/main/java/net/ldm/mo_enchants/block/LiquefyingMagmaBlock.java
+++ src/main/java/net/ldm/mo_enchants/block/LiquefyingMagmaBlock.java
@@ -1,14 +1,15 @@
 
 package net.ldm.mo_enchants.block;
 
-import net.ldm.mo_enchants.enchantment.helpers.LiquefyingMagmaBlockAddedProcedure;
-import net.ldm.mo_enchants.enchantment.helpers.LiquefyingMagmaBlockDestroyedProcedure;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
@@ -16,16 +17,15 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.HitResult;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class LiquefyingMagmaBlock extends Block {
 	public LiquefyingMagmaBlock() {
-		super(Properties.of(Material.STONE).sound(SoundType.STONE).strength(0.5f, 0f).lightLevel(s -> 3).requiresCorrectToolForDrops()
+		super(Properties.of(Material.STONE).sound(SoundType.STONE).strength(0.5f, 0f).lightLevel(s -> 5).requiresCorrectToolForDrops()
 				.hasPostProcess((bs, br, bp) -> true).emissiveRendering((bs, br, bp) -> true).noLootTable());
-	}
-
-	@Override
-	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 15;
 	}
 
 	@Override
@@ -43,13 +43,73 @@ public class LiquefyingMagmaBlock extends Block {
 	@Override
 	public void onPlace(BlockState blockstate, Level world, BlockPos pos, BlockState oldState, boolean moving) {
 		super.onPlace(blockstate, world, pos, oldState, moving);
-		LiquefyingMagmaBlockAddedProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+		added(world, pos.getX(), pos.getY(), pos.getZ());
 	}
 
 	@Override
 	public boolean onDestroyedByPlayer(BlockState blockstate, Level world, BlockPos pos, Player entity, boolean willHarvest, FluidState fluid) {
 		boolean returnValue = super.onDestroyedByPlayer(blockstate, world, pos, entity, willHarvest, fluid);
-		LiquefyingMagmaBlockDestroyedProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+		removed(world, pos.getX(), pos.getY(), pos.getZ());
 		return returnValue;
+	}
+
+	private static void added( LevelAccessor world, double x, double y, double z) {
+		if (world instanceof Level level) {
+			if (!level.isClientSide()) {
+				level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.lava.extinguish")),
+						SoundSource.BLOCKS, (float) 0.6, 1);
+			} else {
+				level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.lava.extinguish")),
+						SoundSource.BLOCKS, (float) 0.6, 1, false);
+			}
+		}
+		new Object() {
+			private int ticks = 0;
+			private float waitTicks;
+			private LevelAccessor world;
+
+			public void start(LevelAccessor world, int waitTicks) {
+				this.waitTicks = waitTicks;
+				MinecraftForge.EVENT_BUS.register(this);
+				this.world = world;
+			}
+
+			@SubscribeEvent
+			public void tick( TickEvent.ServerTickEvent event) {
+				if (event.phase == TickEvent.Phase.END) {
+					this.ticks += 1;
+					if (this.ticks >= this.waitTicks)
+						run();
+				}
+			}
+
+			private void run() {
+				world.setBlock(new BlockPos(x, y, z), Blocks.LAVA.defaultBlockState(), 3);
+				if (world instanceof Level _level) {
+					if (!_level.isClientSide()) {
+						_level.playSound(null, new BlockPos(x, y, z),
+								ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.lava.extinguish")), SoundSource.BLOCKS, (float) 0.6,
+								1);
+					} else {
+						_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.lava.extinguish")),
+								SoundSource.BLOCKS, (float) 0.6, 1, false);
+					}
+				}
+				MinecraftForge.EVENT_BUS.unregister(this);
+			}
+		}.start(world, 80);
+	}
+
+	private static void removed(LevelAccessor world, double x, double y, double z) {
+		world.setBlock(new BlockPos(x, y, z), Blocks.LAVA.defaultBlockState(), 3);
+		if (world instanceof Level level) {
+			if (!level.isClientSide()) {
+				level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.lava.extinguish")),
+						SoundSource.BLOCKS, (float) 0.6, 1);
+			} else {
+				level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.lava.extinguish")),
+						SoundSource.BLOCKS, (float) 0.6, 1, false);
+			}
+		}
 	}
 }
