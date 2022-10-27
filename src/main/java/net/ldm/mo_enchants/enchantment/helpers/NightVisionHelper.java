4358677af6f8851dfda65dac735f1c6f68a05ae5
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-10-26 20:32:02 -0400

    Moved helpers directory into enchantments

---
+++ src/main/java/net/ldm/mo_enchants/enchantment/helpers/NightVisionHelper.java
@@ -0,0 +1,26 @@
+package net.ldm.mo_enchants.enchantment.helpers;
+
+import net.ldm.mo_enchants.init.MoEnchantsEnchantments;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class NightVisionHelper {
+	@SubscribeEvent
+	public static void OnEquipmentChange( LivingEquipmentChangeEvent event ) {
+		if (event.getSlot().equals(EquipmentSlot.HEAD) && EnchantmentHelper.getItemEnchantmentLevel(MoEnchantsEnchantments.NIGHT_VISION.get(), event.getTo()) >= 1 &&
+		event.getEntity() instanceof Player) {
+			event.getEntity().addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 2147483647, 0, false, false, false));
+		}
+
+		if (event.getSlot().equals(EquipmentSlot.HEAD) && EnchantmentHelper.getItemEnchantmentLevel(MoEnchantsEnchantments.NIGHT_VISION.get(), event.getFrom()) >= 1 &&
+				event.getEntity() instanceof Player)
+			event.getEntity().removeEffect(MobEffects.NIGHT_VISION);
+	}
+}
---
+++ src/main/java/net/ldm/mo_enchants/enchantment/helpers/TotemlikeAnimationMainhandProcedure.java
@@ -0,0 +1,17 @@
+package net.ldm.mo_enchants.enchantment.helpers;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.client.Minecraft;
+
+public class TotemlikeAnimationMainhandProcedure {
+	public static void execute(LevelAccessor world, Entity entity) {
+		if (entity == null)
+			return;
+		if (world.isClientSide())
+			Minecraft.getInstance().gameRenderer
+					.displayItemActivation((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY));
+	}
+}
