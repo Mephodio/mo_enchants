c729cc390afa5607600d72810cac396d4ce6f54d
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-10-26 20:18:29 -0400

    Made PanicHelperProcedure easier to read

--- src/main/java/net/ldm/mo_enchants/procedures/PanicHelperProcedure.java
+++ src/main/java/net/ldm/mo_enchants/procedures/PanicHelperProcedure.java
@@ -6,66 +6,44 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import javax.annotation.Nullable;
 
 @Mod.EventBusSubscriber
 public class PanicHelperProcedure {
 	@SubscribeEvent
 	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
 		if (event.phase == TickEvent.Phase.END) {
-			execute(event, event.player.level, event.player);
+			execute(event.player.level, event.player);
 		}
 	}
-
-	public static void execute(LevelAccessor world, Entity entity) {
-		execute(null, world, entity);
-	}
-
-	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
+	
+	private static void execute(LevelAccessor world, Player entity) {
 		if (entity == null)
 			return;
-		if (EnchantmentHelper.getItemEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(),
-				(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY)) != 0
-				&& (entity instanceof LivingEntity _livEnt ? _livEnt.getHealth() : -1) < 4) {
-			if (entity instanceof LivingEntity _entity)
-				_entity.setHealth(5);
-			if (entity instanceof LivingEntity _entity)
-				_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION,
-						(int) (EnchantmentHelper.getItemEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(),
-								(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY))
-								* 200),
-						(int) (EnchantmentHelper.getItemEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(),
-								(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY))
-								+ -1)));
-			if (entity instanceof LivingEntity _entity)
-				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,
-						(int) (EnchantmentHelper.getItemEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(),
-								(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY))
-								* 150),
-						(int) (EnchantmentHelper.getItemEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(),
-								(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY))
-								+ -1)));
+		if (EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(), (entity.getItemBySlot(EquipmentSlot.FEET))) >= 1 && entity.getHealth() < 4) {
+			entity.setHealth(5);
+			entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION,
+					(EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(), (entity.getItemBySlot(EquipmentSlot.FEET))) * 200),
+					(EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(), (entity.getItemBySlot(EquipmentSlot.FEET))))));
+			entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION,
+					(EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(), (entity.getItemBySlot(EquipmentSlot.FEET))) * 150),
+					(EnchantmentHelper.getTagEnchantmentLevel(MoEnchantsEnchantments.PANIC.get(), (entity.getItemBySlot(EquipmentSlot.FEET))))));
 			if (entity instanceof ServerPlayer) {
 				if (world.isClientSide())
 					Minecraft.getInstance().gameRenderer.displayItemActivation(
-							(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY));
-			}
-			{
-				ItemStack _ist = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY);
-				if (_ist.hurt(15, RandomSource.create(), null)) {
-					_ist.shrink(1);
-					_ist.setDamageValue(0);
+							(entity.getItemBySlot(EquipmentSlot.FEET)));
+			} {
+				ItemStack item = (entity.getItemBySlot(EquipmentSlot.FEET));
+				if (item.hurt(15, RandomSource.create(), null)) {
+					item.shrink(1);
+					item.setDamageValue(0);
 				}
 			}
 		}
