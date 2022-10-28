6f54c1ba82b1d7a3f55184c61a93c8f306a0baec
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2022-10-28 17:00:38 -0400

    Swiftness speed is less strong

--- src/main/java/net/ldm/mo_enchants/enchantment/helpers/SwiftnessHelper.java
+++ src/main/java/net/ldm/mo_enchants/enchantment/helpers/SwiftnessHelper.java
@@ -9,9 +9,9 @@ import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import java.util.UUID;
 
 public class SwiftnessHelper {
-	private static final AttributeModifier swiftnessEnchantmentLv1 = new AttributeModifier(UUID.fromString("7a738a54-6d1f-423d-ba6d-282c405c46ea"), "swiftnessEnchantmentLv1", 1, AttributeModifier.Operation.ADDITION);
-	private static final AttributeModifier swiftnessEnchantmentLv2 = new AttributeModifier(UUID.fromString("430f19d0-3003-462d-a368-a7657ce2e34a"), "swiftnessEnchantmentLv2", 2, AttributeModifier.Operation.ADDITION);
-	private static final AttributeModifier swiftnessEnchantmentLv3 = new AttributeModifier(UUID.fromString("af804924-997d-42df-b85b-aa58553fb127"), "swiftnessEnchantmentLv3", 3, AttributeModifier.Operation.ADDITION);
+	private static final AttributeModifier swiftnessEnchantmentLv1 = new AttributeModifier(UUID.fromString("7a738a54-6d1f-423d-ba6d-282c405c46ea"), "swiftnessEnchantmentLv1", 0.01, AttributeModifier.Operation.ADDITION);
+	private static final AttributeModifier swiftnessEnchantmentLv2 = new AttributeModifier(UUID.fromString("430f19d0-3003-462d-a368-a7657ce2e34a"), "swiftnessEnchantmentLv2", 0.02, AttributeModifier.Operation.ADDITION);
+	private static final AttributeModifier swiftnessEnchantmentLv3 = new AttributeModifier(UUID.fromString("af804924-997d-42df-b85b-aa58553fb127"), "swiftnessEnchantmentLv3", 0.03, AttributeModifier.Operation.ADDITION);
 
 	public static void execute(LivingEquipmentChangeEvent event) {
 		if (event.getSlot().equals(EquipmentSlot.FEET)) {
